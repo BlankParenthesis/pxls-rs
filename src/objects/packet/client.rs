faba6bdd400364086a9597f408045d8415fa4c9e
[  ] <blank.parenthesis@gmail.com>
2024-01-30 19:49:28 +0000

    Split packet into separate files

---
+++ src/objects/packet/client.rs
@@ -0,0 +1,17 @@
+use serde::Deserialize;
+use crate::objects::Extension;
+
+#[derive(Deserialize, Debug, Clone)]
+#[serde(tag = "type")]
+#[serde(rename_all = "kebab-case")]
+pub enum Packet {
+	Authenticate { token: Option<String> },
+}
+
+impl From<&Packet> for Extension {
+	fn from(event: &Packet) -> Self {
+		match event {
+			Packet::Authenticate { .. } => Extension::Authentication,
+		}
+	}
+}
